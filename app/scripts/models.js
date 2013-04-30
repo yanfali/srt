49c4949da29bd644df421f9e781df3bf4ffe6044
Yan-Fa Li <yanfali@gmail.com>
2013-04-29 21:35:39 -0700

    startToString, endToString, millisToString impl
    - with unit tests

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,8 +1,14 @@
 /*global define, Backbone */
 define(['marionette', 'underscore'], function(marionette, _) {
     'use strict';
+    var MILLIS_IN_SECOND = 1000,
+        SECONDS_IN_MINUTE = 60,
+        MINUTE_AS_MILLIS = MILLIS_IN_SECOND * SECONDS_IN_MINUTE,
+        MINUTES_IN_HOUR = 60,
+        HOURS_AS_MILLIS = MINUTE_AS_MILLIS * MINUTES_IN_HOUR,
+        HOURS_IN_DAY = 24;
     var subtitle = Backbone.Model.extend({
-        template: '<%= args.secs %>:<%= args.millis %>',
+        template: '<%= args.hours %>:<%= args.mins %>:<%= args.secs %>,<%= args.millis %>',
         defaults: {
             'text': '',
             'start': 0,
@@ -14,12 +20,22 @@ define(['marionette', 'underscore'], function(marionette, _) {
             return str.slice(slen - len, slen);
         },
         startToString: function() {
-            var model = this.toJSON();
-            var millis = model.start % 1000;
-            var secs = model.start % (1000 * 60);
+            return this.millisToString(this.get('start'));
+        },
+        endToString: function() {
+            return this.millisToString(this.get('end'));
+        },
+        millisToString: function(ms) {
+            var millis, secs, minutes, hours;
+            millis = ms % MILLIS_IN_SECOND;
+            secs = ms >= MILLIS_IN_SECOND ? (ms / MILLIS_IN_SECOND) % SECONDS_IN_MINUTE : 0;
+            minutes = ms >= MINUTE_AS_MILLIS ? (ms / MINUTE_AS_MILLIS) % MINUTES_IN_HOUR : 0;
+            hours = ms >= HOURS_AS_MILLIS ? (ms / HOURS_AS_MILLIS) % HOURS_IN_DAY : 0;
             return _.template(this.template, {
-                'millis': this.fmtZeros(millis, 3),
-                'secs': this.fmtZeros(secs, 2)
+                'hours': this.fmtZeros(hours, 2),
+                'mins': this.fmtZeros(minutes, 2),
+                'secs': this.fmtZeros(secs, 2),
+                'millis': this.fmtZeros(millis, 3)
             }, {
                 variable: 'args'
             });
--- test/spec/test.js
+++ test/spec/test.js
@@ -5,9 +5,33 @@ define(function(require) {
     (function() {
         describe('Subtitle model tests', function() {
             describe('startToString output', function() {
-                it('output some formatted strings', function() {
+                it('default values', function() {
                     var subtitle = new models.Subtitle();
-                    subtitle.startToString().should.equal('00:000');
+                    subtitle.startToString().should.equal('00:00:00,000');
+                });
+                it('< 1 second', function() {
+                    var subtitle = new models.Subtitle({start: 999});
+                    subtitle.startToString().should.equal('00:00:00,999');
+                });
+                it('1 second', function() {
+                    var subtitle = new models.Subtitle({start: 1000});
+                    subtitle.startToString().should.equal('00:00:01,000');
+                });
+                it('59 seconds', function() {
+                    var subtitle = new models.Subtitle({start: 59000});
+                    subtitle.startToString().should.equal('00:00:59,000');
+                });
+                it('1 minute', function() {
+                    var subtitle = new models.Subtitle({start: 60000});
+                    subtitle.startToString().should.equal('00:01:00,000');
+                });
+                it('59 minutes', function() {
+                    var subtitle = new models.Subtitle({start: 3540000});
+                    subtitle.startToString().should.equal('00:59:00,000');
+                });
+                it('1 hour', function() {
+                    var subtitle = new models.Subtitle({start: 3600000});
+                    subtitle.startToString().should.equal('01:00:00,000');
                 });
             });
         });
