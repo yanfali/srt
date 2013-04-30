7ce16bb4ae507c04e6de955ad1e19d1e8eaaf7d2
Yan-Fa Li <yanfali@gmail.com>
2013-04-30 14:21:20 -0700

    File Experiments based on HTML5Rocks article
    Working code, but more of a noodle than the final product.
    http://www.html5rocks.com/en/tutorials/file/dndfiles/

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,16 @@
-/*global define, Backbone */
+/*global define, $, Backbone */
 define(['marionette', 'views', 'models'], function(marionette, uilib, models) {
     'use strict';
     var SrtApp = new Backbone.Marionette.Application();
     SrtApp.lib = uilib;
     SrtApp.model = models;
     SrtApp.addRegions({
-        navbar: uilib.regions.NavRegion
+        navbar: uilib.regions.NavRegion,
+        workarea: uilib.regions.MainRegion
     });
     SrtApp.navbar.show(new uilib.views.NavBarView());
+    SrtApp.workarea.show(new uilib.views.WorkAreaView());
+    $('body').on('drop', function(e) { e.preventDefault(); });
     return SrtApp;
 });
 
--- app/scripts/views.js
+++ app/scripts/views.js
@@ -1,4 +1,4 @@
-/*global define, Backbone */
+/*global define, Backbone, escape, _ */
 define(['marionette'], function() {
     'use strict';
     var navBarView = Backbone.Marionette.View.extend({
@@ -22,16 +22,70 @@ define(['marionette'], function() {
             view.setElement($nav[0]);
         }
     });
-    var mainPane = Backbone.Marionette.Region.extend({
-        el: '#workarea'
+    var mainRegion = Backbone.Marionette.Region.extend({
+        el: '#workarea',
+        open: function(view) {
+            var $nav = this.$el.find('.subtitles');
+            console.log('open: adding subtitle view');
+            view.setElement($nav[0]);
+        }
+    });
+    var mainView = Backbone.Marionette.View.extend({
+        ullist: '<ul><%= args.listitems %></ul>',
+        listitem: '<li><%= args.name %> - <%= args.type %> - <%= args.size %></li>',
+        events: {
+            'dragover': 'handleDragOver',
+            'drop': 'handleDrop'
+        },
+        handleDragOver: function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.originalEvent.dataTransfer.dropEffect = 'copy';
+            console.log('drag over!');
+        },
+        handleDrop: function(e) {
+            var files, output, reader;
+            e.stopPropagation();
+            e.preventDefault();
+            console.log('dropped!');
+
+            files = e.originalEvent.dataTransfer.files;
+            output = [];
+            for (var i = 0, f = files[i]; f ; f = files[++i]) {
+                output.push(_.template(this.listitem, {
+                    'name': escape(f.name),
+                    'type': f.type || 'n/a',
+                    'size': f.size
+                }, {
+                    'variable': 'args'
+                }));
+            }
+            this.$('.files').html(_.template(this.ullist, {
+                'listitems': output.join('')
+            }, {
+                'variable': 'args'
+            }));
+
+            reader = new FileReader();
+            reader.onload = (function(){
+                return function(e) {
+                    var lines;
+                    console.log('loaded');
+                    lines = e.target.result.split('\r\n');
+                    console.log('read ' + lines.length);
+                };
+            })(files[0]);
+            reader.readAsText(files[0]);
+        }
     });
     var lib = {
         'regions': {
             NavRegion: navRegion,
-            WorkArea: mainPane
+            MainRegion: mainRegion
         },
         'views': {
-            NavBarView: navBarView
+            NavBarView: navBarView,
+            WorkAreaView: mainView
         }
     };
     return lib;
