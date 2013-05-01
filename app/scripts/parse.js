fca588755f71a7c73c73861fe09fc2109a1ee371
Yan-Fa Li <yanfali@gmail.com>
2013-04-30 23:00:09 -0700

    WIP - SRT format parser
    Passes initial tests. Needs a bunch of refactoring and tests involving
    longer sequences of SRT.
    
    Uses jQuery Deferred for the API. I want to experiment with making async
    APIs using only Deferreds to understand the advantages and
    disadvantages. If this works I may try out Q.
    
    The fromSrt function accepts two parameters, one an array of strings
    containing the source data, two the collection to add the Subtitles too.
    
    It returns a jQuery promise and runs async. It's not clear if this will
    take a long time for the average SRT file. If it does, the task may have
    to add progress calls to yield after a certain number of SRTs have been
    created.
    
    The done callback returns the number of lines parsed and the collection.
    The fail callback returns the error.

--- app/scripts/parse.js
+++ app/scripts/parse.js
@@ -1,14 +1,76 @@
-/*global define */
-define(['marionette', 'underscore'], function() {
+/*global define,$, setTimeout */
+define(['marionette', 'underscore', 'models'], function(m, _, models) {
     'use strict';
-    var srt = function(strings, collection) {
-        if (collection && collection.models.length > 0) {
-            console.log('Got a collection with models in it');
-        }
-        return strings.length;
+    var EOL = '\r\n',
+        lineRegex = /^\d+$/,
+        timestampRegex = (/(\d{2}:\d{2}:\d{2},\d{3})\s+-->\s+(\d{2}:\d{2}:\d{2},\d{3})/),
+        srt, tsToMs = models.Subtitle.prototype.timestampToMillis;
+    srt = function(strings, collection) {
+        var d = $.Deferred(),
+            inner;
+        inner = function() {
+            var nolines, foundrealend;
+            if (collection && collection.models) {
+                console.log('Got a collection with models in it');
+            }
+            nolines = strings.length;
+            foundrealend = false;
+            for (var i = nolines - 1; i > -1; --i) {
+                if (strings[i] === EOL && strings[i - 1] === EOL) {
+                    foundrealend = true;
+                    nolines = i - 1;
+                    break;
+                }
+            }
+            if (!foundrealend) {
+                d.reject(new Error('Does not look like a valid file'));
+                return;
+            }
+
+            var obj = {};
+            var inSrt = false;
+            var matches;
+            for (i = 0; i < nolines; i++) {
+                var line = strings[i];
+                line = line.length > 2 ? line.substring(0, line.length - 2) : line;
+                console.log('line ' + i + ' @ ' + (line || ''));
+                if ((!inSrt) && lineRegex.test(line)) {
+                    inSrt = true;
+                    console.log('found an new subtitle');
+                    obj.line = line;
+                    continue;
+                }
+                if (inSrt) {
+                    if (line === '\r\n') {
+                        collection.add({
+                            'text': obj.text,
+                            'start': obj.start,
+                            'end': obj.end
+                        });
+                        obj = {};
+                        inSrt = false;
+                        continue;
+                    }
+                    matches = timestampRegex.exec(line);
+                    if (matches) {
+                        obj.start = tsToMs(matches[1]);
+                        obj.end = tsToMs(matches[2]);
+                        continue;
+                    }
+                    if (obj.text) {
+                        obj.text.push(line);
+                    } else {
+                        obj.text = [line];
+                    }
+                }
+            }
+            d.resolve(nolines, collection);
+        };
+        setTimeout(inner, 0);
+        return d.promise();
     };
     var lib = {
-        'fromSrt': srt
+        fromSrt: srt
     };
     return lib;
 });
--- test/spec/parse_tests.js
+++ test/spec/parse_tests.js
@@ -1,15 +1,53 @@
-/*global define, describe, it */
-/*jshint multistr: true */
+/* global define, describe, it, should */
+/* jshint multistr: true */
 'use strict';
 
-var dataset = [ '1\r\n', '00:00:00,242 --> 00:00:03,703\r\n', 'Book One: Water | Chapter Twenty:\r\n', 'The Siege of the North Part 2\r\n' ];
+var dataset = [ '1\r\n', '00:00:00,242 --> 00:00:03,703\r\n', 'Book One: Water | Chapter Twenty:\r\n', 'The Siege of the North Part 2\r\n', '\r\n', '\r\n', '\r\n', '0\r\n', '00:00:00,242 --> 00:00:03,703\r\n', 'weird junk on the end\r\n' ];
+
+var datasetBad = [ '1\r\n' ];
+var datasetEmpty = [ ];
+
+
 define(function(require) {
     var parse = require('parse');
+    var model = require('models');
+    var subs = new model.Subtitles();
     (function() {
         describe('SRT Parser Tests', function() {
             describe('Testing the parsing function', function() {
-                it('default values', function() {
-                    parse.fromSrt(dataset).should.equal(4);
+                it('find real length of file', function() {
+                    subs.reset();
+                    parse.fromSrt(dataset, subs).done(function(lines) {
+                        lines.should.equal(5);
+                    }).fail(function(err) {
+                        console.log(err);
+                        should.not.exist(err);
+                    });
+                });
+                it('bad dataset', function() {
+                    subs.reset();
+                    parse.fromSrt(datasetBad, subs).fail(function(err) {
+                        err.message.should.equal('Does not look like a valid file');
+                    });
+                });
+                it('empty dataset', function() {
+                    subs.reset();
+                    parse.fromSrt(datasetEmpty, subs).fail(function(err) {
+                        err.message.should.equal('Does not look like a valid file');
+                    });
+                });
+                it('add a model', function() {
+                    subs.reset();
+                    parse.fromSrt(dataset, subs).fail(function(err) {
+                        err.message.should.not.exist(err);
+                    }).done(function(lines, collection){
+                        lines.should.equal(5);
+                        collection.length.should.equal(1);
+                        var model = collection.at(0);
+                        model.get('start').should.equal(242);
+                        model.get('end').should.equal(3703);
+                        model.get('text').length.should.equal(2);
+                    });
                 });
             });
         });
