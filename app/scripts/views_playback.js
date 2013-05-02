ae1ac2906921ee2e35a0ac5c90cfa4c8c98e2348
Yan-Fa li <yanfali@gmail.com>
2013-05-01 23:50:00 -0700

    Rough and ready proof of concept player
    This is a working player, but it's a proof of concept.
    
    It has a number of drawbacks. It can't handle overlapping subtitles. I'm
    not sure it's in the spec I have to check. It can't pause and resume.
    It plays even when subtitles aren't loaded. Stepping doesn't
    work yet.
    
    I'm planning on moving the timer code into it's own event based trigger.
    Same thing for the player. Marionette has an event based system
    built in.
    
    The button controls are also flakey.
    
    Yuhri would also like me to stream the video at the same time.
    This will probably require live transcoding.

--- app/scripts/views_playback.js
+++ app/scripts/views_playback.js
@@ -1,5 +1,5 @@
 /*global define, Backbone, $ */
-define(['marionette', 'models', 'views_subtitle'], function() {
+define(['marionette', 'underscore', 'models', 'views_subtitle'], function(m, _, models) {
     'use strict';
     var playbackRegion = Backbone.Marionette.Region.extend({
         el: '#playback',
@@ -9,10 +9,17 @@ define(['marionette', 'models', 'views_subtitle'], function() {
             view.setElement($nav[0]);
         }
     });
+    var proto = models.Subtitle.prototype;
+    var msToTs = function(ms) {
+            return proto.millisToString.call(proto, ms);
+        };
+
     var playbackView = Backbone.Marionette.View.extend({
         events: {
             'click .controls': 'handleClick'
         },
+        timer: null,
+        subtimer: null,
         ui: {
             timer: '.timer',
             playtext: '.play-text'
@@ -23,9 +30,51 @@ define(['marionette', 'models', 'views_subtitle'], function() {
             var $target = $(e.target);
             if ($target.is('a.btn\\ play') || $target.is('i.icon-play')) {
                 console.log('play');
+                if (this.timer === null) {
+                    this.stopwatch = new Date().getTime();
+                    var $timer = $(this.ui.timer);
+                    var self = this;
+                    self.timer = setInterval(function() {
+                        var ms = new Date().getTime() - self.stopwatch;
+                        $timer.text(msToTs(ms));
+                    }, 100);
+
+                    var index = 0;
+                    var subtitle = this.collection.at(index++);
+                    var nextMs = subtitle.get('start');
+                    console.log(nextMs);
+                    var $playtext = $(this.ui.playtext);
+                    var playerF = function(subtitle) {
+                            $playtext.html(subtitle.get('text').join('</br>'));
+                            var start = subtitle.get('start');
+                            var end = subtitle.get('end');
+                            setTimeout(function() {
+                                $playtext.text('');
+                            }, end - start);
+
+                            subtitle = self.collection.at(index++);
+                            var newstart = subtitle.get('start');
+                            var elapsed = new Date().getTime() - self.stopwatch;
+                            self.subtimer = setTimeout(function() {
+                                playerF(subtitle);
+                            }, newstart - elapsed);
+                        };
+
+                    this.subtimer = setTimeout(function() {
+                        playerF(subtitle);
+                    }, nextMs);
+                }
             }
             if ($target.is('a.btn\\ stop') || $target.is('i.icon-stop')) {
                 console.log('stop');
+                if (this.timer) {
+                    clearInterval(this.timer);
+                    this.timer = null;
+                }
+                if (this.subtimer) {
+                    clearTimeout(this.subtimer);
+                    this.subtimer = null;
+                }
             }
             if ($target.is('a.btn\\ step-backward') || $target.is('i.icon-step-backward')) {
                 console.log('back');
@@ -40,6 +89,7 @@ define(['marionette', 'models', 'views_subtitle'], function() {
             } else {
                 throw new Error('Need a collection!');
             }
+            _.bindAll(this);
         }
     });
     var lib = {
