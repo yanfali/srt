312cd93399012f8a9941074e04dcfa804c895f72
Yan-Fa Li <yanfali@gmail.com>
2013-05-04 22:40:32 -0700

    Address code comments
    - extract subtitle template into index.html
    - use bindUIElements
    - rename template -> serializeData and provide custom deserialization
      for view. Less code to maintain or think about.
    - switch to model.msToTemplate

--- app/scripts/views_subtitle.js
+++ app/scripts/views_subtitle.js
@@ -1,33 +1,30 @@
 /*global define, Backbone */
 define(['marionette', 'underscore', 'models'], function(marionette, _, models) {
     'use strict';
-    var msToTs = models.Subtitle.prototype.millisToString;
-    var subtitleTemplateHtml = ['<span class="start<%= args.clazz %>"><%= args.start %></span>', '<span class="end"><%= args.end %></span>', '<span class="text"><%= args.text %></span>'];
-    subtitleTemplateHtml = subtitleTemplateHtml.join('');
-    var subtitleTemplate = _.template(subtitleTemplateHtml, null, {
-        variable: 'args'
-    });
+    var msToTs = models.msToTimestamp;
     var subtitleView = Backbone.Marionette.ItemView.extend({
+        template: '#subtitle-template',
         modelEvents: {
             'change:selected': 'changeSelected'
         },
+        ui: {
+            'text': '.text'
+        },
         changeSelected: function() {
             if (this.model.get('selected')) {
-                this.$('.text').addClass('selected');
+                this.ui.text.addClass('selected');
             } else {
-                this.$('.text').removeClass('selected');
+                this.ui.text.removeClass('selected');
             }
         },
-        template: function(serModel) {
-            console.log(serModel);
-            var start = msToTs.call(models.Subtitle.prototype, serModel.start);
-            var end = msToTs.call(models.Subtitle.prototype, serModel.end);
-            return subtitleTemplate({
-                start: start,
-                end: end,
-                text: serModel.text.join('</br>'),
-                clazz: serModel.selected ? 'current' : ''
-            });
+        serializeData: function() {
+            var m = this.model;
+            return {
+                start: msToTs(m.get('start')),
+                end: msToTs(m.get('end')),
+                text: m.get('text').join('</br>'),
+                clazz: m.get('selected') ? 'current' : ''
+            };
         }
     });
     var lib = {
